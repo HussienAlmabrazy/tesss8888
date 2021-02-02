afce429591641dff56a66a7215c4f7a8d9175736
HUSSIEN <HUSSIEN@DESKTOP-4A0SALD>
2021-02-03 00:17:44 +0300

    kdkldhljkd lidkjk

---
+++ ConsoleApplication5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace CSharpTutorial
+{
+    class Program
+    {
+        static void fun()
+        {
+
+        }
+        public static void Main()
+        {
+            Thread t = new Thread(fun);
+            Console.WriteLine(t.ThreadState);
+            t.Name = "the5";
+            t.IsBackground = true;
+            t.Start();
+            Console.WriteLine(t.ThreadState);
+            t.Join();
+            Console.WriteLine(t.ThreadState);
+            if (!t.IsAlive)
+            {
+                t.Start();
+            }
+            Console.WriteLine(t.IsBackground);
+            Console.ReadLine();
+        }
+
+    }
+       
+    
+}
